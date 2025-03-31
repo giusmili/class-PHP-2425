9b937b82b34848aa14bea4d8c05de3e81e4e055d
giusmili <giusmili67@gmail.com>
2025-03-31 10:43:56 +0200

    views

---
+++ views/home.php
@@ -0,0 +1,48 @@
+<?php 
+	include_once './controller/config.php';
+	
+	# inclusion de la configuration /* */
+
+	include_once  './partial/head.inc.php';
+
+	# head
+
+?>
+
+
+<body>
+
+<?php
+	
+	include_once './partial/header.inc.php';
+
+	# header
+?>
+<main>
+    <?php
+
+	include_once './partial/main.inc.php';
+
+	# main zone
+	 
+	?>
+</main>
+	<?php
+
+	include_once './partial/footer.inc.php';
+
+	# footer
+
+	?>
+	
+	<!-- footer -->
+
+	<!--cours https://hackmd.io/iNktz6XzRyu7gGmKvPpXfQ?view-->
+	<!-- <pre>
+		<?php
+			/* print phpInfo() */
+
+			// print_r($_SERVER)
+		?>
+	</pre> -->
+</html>
